22b738cc47c2b5917f24c450bf78fa7a6a072da5
Thomas Mao <tm1813@gmail.com>
2021-07-21 09:02:46 +0800

    initial update

---
+++ test/epluishadowcljs1/core_test.clj
@@ -0,0 +1,7 @@
+(ns epluishadowcljs1.core-test
+  (:require [clojure.test :refer :all]
+            [epluishadowcljs1.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
